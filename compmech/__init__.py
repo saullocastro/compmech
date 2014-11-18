065a17a814c34edda0d93d04155013033ee30404
saullogiovani <saullogiovani@gmail.com>
2014-11-18 08:29:37 +0100

    DOC: added header

--- compmech/__init__.py
+++ compmech/__init__.py
@@ -1,2 +1,18 @@
+r"""
+=========================================
+Computational Mechanics (:mod:`compmech`)
+=========================================
+
+.. currentmodule:: compmech
+
+Python-based toolkit for computational mechanics.
+
+The compmech package contains semi-analytical routines for linear buckling,
+linear static and non-linear static analysis of unstiffened cylindrical and
+conical shells (:mod:`.compmech.conecyl`), and panels
+(:mod:`.compmech.panels`). The implementation is based on Python and the low
+level routines are programmed in Cython.
+
+"""
 __version__ = '0.3.2'
 
