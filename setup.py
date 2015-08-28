66ef07ac199addf6ef02d8402cca09517030c981
Laboratório de Mecânica de Estrututras <LMEst@MacBook-Pro-de-Laboratorio.local>
2015-08-28 12:24:20 -0300

    BUG: installation more robust to find numpy include dir

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from glob import glob
 import sys
 import os
 from subprocess import Popen
+import numpy
 
 #params = 'build_ext -inplace -IC:\clones\cubature\cubature ' + ' '.join(sys.argv[1:])
-params = 'build_ext --inplace ' + ' '.join(sys.argv[1:]) + ' clean'
+params = 'build_ext --inplace -I%s' % numpy.get_include() + ' '.join(sys.argv[1:]) + ' clean'
 
 cwd = os.getcwd()
 
@@ -12,18 +13,20 @@ print('####################')
 print('Compiling modules...')
 print('####################')
 print('')
-basedirs = [os.path.join('compmech', 'conecyl', 'clpt'),
+basedirs = [
+            os.path.join('compmech', 'conecyl', 'clpt'),
             os.path.join('compmech', 'conecyl', 'fsdt'),
             os.path.join('compmech', 'integrate'),
             os.path.join('compmech', 'conecyl', 'imperfections'),
             os.path.join('compmech', 'aero', 'pistonplate', 'clpt'),
+            os.path.join('compmech', 'aero', 'pistonstiffpanel', 'clpt'),
            ]
 
 for basedir in basedirs:
     print('Compiling setup.py in %s' % basedir)
     basedir = os.path.sep.join([cwd, basedir])
     os.chdir(basedir)
-    for fname in glob('setup_*.py'):
+    for fname in glob('setup*.py'):
         p = Popen(('python {} '.format(fname) + params), shell=True)
         p.wait()
 os.chdir(cwd)
