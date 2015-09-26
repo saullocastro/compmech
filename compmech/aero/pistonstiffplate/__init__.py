14f7c2e734f5995da41634b1c1e02fb3478edafa
saullocastro <saullogiovani@gmail.com>
2015-09-26 12:45:17 -0300

    DEV: added pistonstiffplate

---
+++ compmech/aero/pistonstiffplate/__init__.py
@@ -0,0 +1,15 @@
+r"""
+=========================================================================
+Piston Theory in Stiffened Plates (:mod:`compmech.aero.pistonstiffplate`)
+=========================================================================
+
+.. currentmodule:: compmech.aero.pistonstiffplate
+
+.. autoclass:: AeroPistonStiffPlate
+    :members:
+
+.. autoclass:: Stiffener
+    :members:
+
+"""
+from pistonstiffplate import load, AeroPistonStiffPlate, Stiffener
