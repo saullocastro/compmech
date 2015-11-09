9e9802502d83032a13503f037d47f00e2f323b56
saullocastro <saullogiovani@gmail.com>
2015-11-09 21:42:32 -0200

    DEV: pistonstiff2Dpanelbay

---
+++ compmech/aero/pistonstiff2Dpanelbay/modelDB.py
@@ -0,0 +1,26 @@
+r"""
+Used to configure the main parameters for each implemented model.
+
+.. currentmodule:: compmech.aero.pistonstiffpanel.modelDB
+
+
+"""
+import numpy as np
+from scipy.sparse import coo_matrix
+
+from clpt import *
+
+db = {
+    'clpt_donnell_bc1': {
+                    'linear static': False,
+                    'linear buckling': True,
+                    'non-linear static': False,
+                    'commons': clpt_commons_bc1,
+                    'linear': clpt_donnell_bc1_linear,
+                    'non-linear': None,
+                    'dofs': 3,
+                    'e_num': 6,
+                    'num': 3,
+                    'num1': 4,
+                    },
+    }
