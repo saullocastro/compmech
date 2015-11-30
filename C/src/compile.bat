422fa3a5071f418a142fb17c6d0977b81115e219
saullocastro <saullogiovani@gmail.com>
2015-11-29 23:40:21 -0200

    ENH: O2 options

--- C/src/compile.bat
+++ C/src/compile.bat
@@ -1,3 +1,3 @@
-cl /LD bardell.c -o../lib/bardell.dll 
-cl /LD bardell_12.c -o../lib/bardell_12.dll 
-cl /LD bardell_functions.c -o../lib/bardell_functions.dll 
+cl /O2 /LD bardell.c -o../lib/bardell.dll 
+cl /O2 /LD bardell_12.c -o../lib/bardell_12.dll 
+cl /O2 /LD bardell_functions.c -o../lib/bardell_functions.dll 
