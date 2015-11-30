386038bdc8e2ba8c60077399328aa0cd47c89b58
saullocastro <saullogiovani@gmail.com>
2015-11-30 00:29:42 -0200

    ENH: compiling using dynamic C libraries

---
+++ compmech/aero/pistonstiff2Dpanelbay/clpt/setup.bat
@@ -0,0 +1,1 @@
+python setup.py build_ext -IC:\repos\compmech\C\include -LC:\repos\compmech\C\lib --inplace clean 
--- compmech/aero/pistonstiff2Dpanelbay/clpt/setup.py
+++ compmech/aero/pistonstiff2Dpanelbay/clpt/setup.py
@@ -3,22 +3,20 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-import sys
-sys.path.append(r'C:\repos\compmech\compmech\func\bardell')
-
 if os.name == 'nt':
-    args_linear = ['/openmp', '/O2', '/favor:INTEL64']
-    args_nonlinear = ['/openmp', '/O2', '/favor:INTEL64', '/fp:fast']
+    args_linear = ['/O2', '/openmp', '/favor:INTEL64']
+    args_nonlinear = ['/O2', '/openmp', '/favor:INTEL64', '/fp:fast']
 else:
     args_linear = ['-O3']
     args_nonlinear = ['-O3']
 
 extensions = [Extension('clpt_donnell_bardell_linear',
-                        ['clpt_donnell_bardell_linear.pyx',
-                         '../../../../C/bardell/bardell.c',
-                         '../../../../C/bardell/bardell_12.c',
-                         '../../../../C/bardell/bardell_functions.c',
-                        ]),
+                        ['clpt_donnell_bardell_linear.pyx'],
+                        libraries=['bardell', 'bardell_12',
+                        'bardell_functions'],
+                        language='c',
+                        extra_compile_args=args_linear,
+                        ),
              ]
 
 ext_modules = cythonize(extensions)
