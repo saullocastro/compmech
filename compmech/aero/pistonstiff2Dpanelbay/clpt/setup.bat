4ba837dd22157eb5ad9ebd82a6376093bfd88c8e
saullocastro <saullogiovani@gmail.com>
2015-12-02 21:58:55 -0200

    MAINT: forcing compilation since it is fast

--- compmech/aero/pistonstiff2Dpanelbay/clpt/setup.bat
+++ compmech/aero/pistonstiff2Dpanelbay/clpt/setup.bat
@@ -1,1 +1,1 @@
-python setup.py build_ext -IC:\repos\compmech\C\include -LC:\repos\compmech\C\lib --inplace clean 
+python setup.py build_ext -IC:\repos\compmech\C\include -LC:\repos\compmech\C\lib --force --inplace clean 
