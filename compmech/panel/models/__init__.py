889afea9df0456103e19f4bf10aa3f1935ff8bac
saullocastro <sgcastro@PC423469.corp.ad.emb>
2015-12-07 15:01:26 -0200

    MAINT: unifying Panel modules

---
+++ compmech/panel/models/__init__.py
@@ -0,0 +1,45 @@
+r"""
+=============================================
+Panel models (:mod:`compmech.panel.models`)
+=============================================
+
+.. currentmodule:: compmech.panel.models
+
+The first part of the module identification is:
+
+- cpanel - Cylindrical Panels
+- kpanel - Conical Panels
+- plate - Flat plates
+
+The second part:
+
+- clt - Classical Laminated Theory
+- fsdt - First-order Shear Deformation Theory
+
+Third part:
+
+- Donnell - kinematic equations using Donnell's terms
+- Sanders - kinematic equations using Sanders' terms
+
+Fourth part:
+- field - functions to calculate field variables
+- empty - functions to calculate the stiffness matrices
+
+"""
+modules = [
+          'cpanel_clpt_donnell_bardell_field',
+          'cpanel_clpt_donnell_bardell',
+          'kpanel_clpt_donnell_bardell_field',
+          'kpanel_clpt_donnell_bardell',
+          'plate_clpt_donnell_bardell_field',
+          'plate_clpt_donnell_bardell',
+          ]
+
+for module in modules:
+    try:
+        exec('from . import {0}'.format(module))
+    except:
+        print('WARNING - module {0} could not be imported!'.format(module))
+        exec('{0} = None'.format(module))
+
+
