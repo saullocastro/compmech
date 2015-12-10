0dfe8e2ae98db8963d87c17f89e37fa2de9e95ae
saullocastro <saullogiovani@gmail.com>
2015-12-10 20:35:03 -0200

    DEV: added all existing models

--- compmech/panel/modelDB.py
+++ compmech/panel/modelDB.py
@@ -8,6 +8,16 @@ Used to configure the main parameters for each implemented model.
 from models import *
 
 db = {
+    'cpanel_clt_donnell_bardell': {
+                    'linear static': True,
+                    'linear buckling': True,
+                    'non-linear static': False,
+                    'field': plate_clt_donnell_bardell_field,
+                    'matrices': cpanel_clt_donnell_bardell,
+                    'dofs': 3,
+                    'e_num': 6,
+                    'num': 3,
+                    },
     'plate_clt_donnell_bardell': {
                     'linear static': True,
                     'linear buckling': True,
@@ -17,5 +27,15 @@ db = {
                     'dofs': 3,
                     'e_num': 6,
                     'num': 3,
+                    },
+    'plate_clt_donnell_bardell_w': {
+                    'linear static': True,
+                    'linear buckling': True,
+                    'non-linear static': False,
+                    'field': plate_clt_donnell_bardell_w_field,
+                    'matrices': plate_clt_donnell_bardell_w,
+                    'dofs': 1,
+                    'e_num': 6,
+                    'num': 1,
                     },
     }
