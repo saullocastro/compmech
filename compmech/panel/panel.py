BUG: fixed calculation for fkM

compmech/panel/panel.py
@@ -53,6 +53,7 @@ class Panel(object):
         self.stack = stack
         self.plyt = plyt
         self.laminaprop = laminaprop
+        self.d = 0.
 
         self.name = ''
         self.bay = None
@@ -522,8 +523,8 @@ class Panel(object):
         if size is None:
             size = self.get_size()
 
+        h = sum(self.plyts)
         if y1 is not None and y2 is not None:
-            h = sum(self.plyts)
             kM = matrices.fkMy1y2(y1, y2, self.mu, self.d, h,
                                   self.a, self.b, self.m, self.n,
                                   self.u1tx, self.u1rx, self.u2tx, self.u2rx,
@@ -534,14 +535,15 @@ class Panel(object):
                                   self.w1ty, self.w1ry, self.w2ty, self.w2ry,
                                   size, row0, col0)
         else:
-            kM = fkM(mu, d, h, a, b, m, n,
-                self.u1tx, self.u1rx, self.u2tx, self.u2rx,
-                self.v1tx, self.v1rx, self.v2tx, self.v2rx,
-                self.w1tx, self.w1rx, self.w2tx, self.w2rx,
-                self.u1ty, self.u1ry, self.u2ty, self.u2ry,
-                self.v1ty, self.v1ry, self.v2ty, self.v2ry,
-                self.w1ty, self.w1ry, self.w2ty, self.w2ry,
-                size, row0, col0)
+            kM = matrices.fkM(self.mu, self.d, h,
+                              self.a, self.b, self.m, self.n,
+                              self.u1tx, self.u1rx, self.u2tx, self.u2rx,
+                              self.v1tx, self.v1rx, self.v2tx, self.v2rx,
+                              self.w1tx, self.w1rx, self.w2tx, self.w2rx,
+                              self.u1ty, self.u1ry, self.u2ty, self.u2ry,
+                              self.v1ty, self.v1ry, self.v2ty, self.v2ry,
+                              self.w1ty, self.w1ry, self.w2ty, self.w2ry,
+                              size, row0, col0)
 
         if finalize:
             assert np.any(np.isnan(kM.data)) == False