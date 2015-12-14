6d6076c6231dee7508994937b38cda74cb8cf358
saullocastro <saullogiovani@gmail.com>
2015-12-14 16:50:19 -0200

    TST: implementing test for freq()

--- compmech/panel/test_panel.py
+++ compmech/panel/test_panel.py
@@ -51,5 +51,23 @@ def test_lb():
         assert np.allclose(p.eigvals[0], 13.9421987614)
 
 
+def test_freq():
+    p = Panel()
+    p.a = 1.
+    p.b = 0.5
+    p.r = p.b*10
+    p.stack = [0, 90, -45, +45]
+    p.plyt = 1e-3*0.125
+    p.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
+    p.model = 'cpanel_clt_donnell_bardell'
+    p.mu = 1.3e3
+    p.m = 10
+    p.n = 11
+
+    p.freq(sparse_solver=False)
+
+
+
 if __name__ == '__main__':
     test_lb()
+    test_freq()
