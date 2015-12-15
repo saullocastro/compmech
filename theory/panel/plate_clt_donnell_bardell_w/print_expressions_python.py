01062ecd62b6a6e12a5107f0c9a50b83d894ff60
saullocastro <saullogiovani@gmail.com>
2015-12-15 10:01:30 -0200

    DEV: kM, kA, cA for this model

---
+++ theory/panel/plate_clt_donnell_bardell_w/print_expressions_python.py
@@ -0,0 +1,46 @@
+import os
+import glob
+from ast import literal_eval
+
+import numpy as np
+import sympy
+from sympy import pi, sin, cos, var
+
+from compmech.conecyl.sympytools import mprint_as_sparse
+
+var('D11, D12, D16, D22, D26, D66')
+var('fAwxixi, fBwxixi, gAw, gBw, fAwxi, fBwxi, gAweta, gBweta')
+var('fAw, fBw, gAwetaeta, gBwetaeta')
+var('a, b, Nxx, Nyy, Nxy')
+var('aeromu, beta, h, mu, d')
+
+subs = {
+       }
+
+def List(*e):
+    return list(e)
+
+for i, filepath in enumerate(
+        glob.glob(r'.\output_expressions_mathematica\fortran_*.txt')):
+    print(filepath)
+    with open(filepath) as f:
+        filename = os.path.basename(filepath)
+        names = filename[:-4].split('_')
+        lines = [line.strip() for line in f.readlines()]
+        string = ''.join(lines)
+        string = string.replace('\\','')
+        tmp = eval(string)
+        matrix = sympy.Matrix(np.atleast_2d(tmp))
+        printstr = ''
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                if matrix[i,j] == 0:
+                    continue
+                else:
+                    printstr += '%s[row+%d, col+%d] = %s\n' % (names[1], i, j, str(matrix[i, j]))
+        printstr = mprint_as_sparse(matrix, names[1], "11",
+                                    print_file=False, collect_for=None,
+                                    subs=subs)
+
+    with open('.\\output_expressions_python\\' + filename, 'w') as f:
+        f.write(printstr)
