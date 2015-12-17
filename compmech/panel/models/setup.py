4497a8ef7804e36b544f6f7c5800b12071d18cf1
saullocastro <saullogiovani@gmail.com>
2015-12-17 01:05:19 +0000

    BLD: removing openmp for gcc, giving an error

--- compmech/panel/models/setup.py
+++ compmech/panel/models/setup.py
@@ -9,8 +9,8 @@ def configuration(parent_package='', top_path=None):
         args_linear = ['/openmp']
         args_nonlinear = ['/openmp', '/fp:fast']
     else:
-        args_linear = ['-fopenmp']
-        args_nonlinear = ['-fopenmp', '-ffast-math']
+        args_linear = []
+        args_nonlinear = ['-ffast-math']
 
     config = Configuration('models', parent_package, top_path)
 
