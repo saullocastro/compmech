0cc05cbb2598f12af4e2ec1fe41f2c7f3627bb3f
saullocastro <saullogiovani@gmail.com>
2016-01-10 22:08:03 -0200

    BLD: fixing compile call

--- continuous-integration/appveyor/cmd_in_env.cmd
+++ continuous-integration/appveyor/cmd_in_env.cmd
@@ -23,6 +23,7 @@ IF "%PYTHON_ARCH%"=="64" (
     call %COMMAND_TO_RUN% || EXIT 1
 ) ELSE (
     ECHO Configuring Windows SDK %WINDOWS_SDK_VERSION% for Python %MAJOR_PYTHON_VERSION% on a 32 bit architecture
+    "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
     "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x86 /release
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
