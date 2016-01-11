fe0c07dbb9f6a4728a7af864c39bd8478b0a7dbd
saullocastro <saullogiovani@gmail.com>
2016-01-11 17:08:46 -0200

    BLD: using gcc instead of cl in AppVeyor

--- compmech/lib/setup.py
+++ compmech/lib/setup.py
@@ -6,6 +6,12 @@ import hashlib
 from multiprocessing import Pool, cpu_count
 from functools import partial
 
+def in_appveyor_ci():
+    if os.environ.get('APPVEYOR_PROJECT_NAME', None) is None:
+        return False
+    else:
+        return True
+
 
 def compile(config, src):
     libdir = realpath(config.package_path)
@@ -19,7 +25,7 @@ def compile(config, src):
     hashpath = srcpath + '.hashcode'
     hash_new = hashlib.sha256(os.name + open(srcpath, 'rb').read()).digest()
 
-    if os.name == 'nt':
+    if os.name == 'nt' and not in_appveyor_ci():
         objext = 'obj'
     else:
         objext = 'o'
@@ -38,7 +44,7 @@ def compile(config, src):
     if needscompile:
         bkpdir = os.getcwd()
         os.chdir(srcdir)
-        if os.name == 'nt':
+        if os.name == 'nt' and not in_appveyor_ci():
             os.system('cl /Ox /c {0}'.format(basename(srcpath)))
         else:
             os.system('gcc -pthread -g -O3 -fPIC -g -c -Wall {0}'.format(basename(srcpath)))
@@ -54,7 +60,7 @@ def compile(config, src):
 def link(config, instlib):
     objs = ''
     libdir = realpath(config.package_path)
-    if os.name == 'nt':
+    if os.name == 'nt' and not in_appveyor_ci():
         objext = 'obj'
     else:
         objext = 'o'
@@ -66,7 +72,7 @@ def link(config, instlib):
             srcpath = join(realpath(config.top_path), src)
         objs += srcpath.replace('.c', '.' + objext) + ' '
 
-    if os.name == 'nt':
+    if os.name == 'nt' and not in_appveyor_ci():
         libpath = join(libdir, instlib[0] + '.dll')
         os.system('link /DLL {0} /OUT:{1}'.format(objs, libpath))
     else:
@@ -99,7 +105,7 @@ def configuration(parent_package='', top_path=None):
     config.make_config_py()
 
     for instlib in config.libraries:
-        if os.environ.get('APPVEYOR_PROJECT_NAME', None) is not None:
+        if in_appveyor_ci():
             for src in instlib[1]['sources']:
                 compile(config, src)
             link(config, instlib)
