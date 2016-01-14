7cf017a2bb1e1d0484fdbf42fdefd0f2d5473aef
saullocastro <saullogiovani@gmail.com>
2016-01-14 16:54:12 -0200

    TST: improved test example to check the routine [ci skip]

--- FORTRAN/bardell/linear_buckling_cpanelbay/verification_stiffpanelbay.py
+++ FORTRAN/bardell/linear_buckling_cpanelbay/verification_stiffpanelbay.py
@@ -9,20 +9,30 @@ spb.plyt = 1e-3*0.125
 spb.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
 spb.model = 'cpanel_clt_donnell_bardell'
 spb.mu = 1.3e3
-spb.m = 11
-spb.n = 12
+spb.m = 13
+spb.n = 14
 
-spb.add_panel(y1=0, y2=spb.b/2., plyt=spb.plyt, Nxx=-100.)
-spb.add_panel(y1=spb.b/2., y2=spb.b, plyt=spb.plyt, Nxx=-100.)
+spb.add_panel(y1=0, y2=spb.b/3., plyt=spb.plyt, Nxx=-100.)
+spb.add_panel(y1=spb.b/3., y2=2*spb.b/3., plyt=spb.plyt, Nxx=-100.)
+spb.add_panel(y1=2*spb.b/3., y2=spb.b, plyt=spb.plyt, Nxx=-100.)
 
-#spb.add_bladestiff1d(ys=spb.b/2., Fx=-10., bf=0.05, fstack=[0, 90, 90, 0],
-        #fplyt=spb.plyt, flaminaprop=spb.laminaprop)
+spb.add_bladestiff1d(ys=spb.b/3., Fx=-100., bf=0.05,
+                     fstack=[0, 90, 90, 0]*4, fplyt=spb.plyt,
+                     flaminaprop=spb.laminaprop)
+spb.add_bladestiff1d(ys=2*spb.b/3., Fx=-100., bf=0.05,
+                     fstack=[0, 90, 90, 0]*4, fplyt=spb.plyt,
+                     flaminaprop=spb.laminaprop)
 
 spb.lb(silent=False)
 
-print spb.panels[0].lam.A
-print spb.panels[0].lam.B
-print spb.panels[0].lam.D
+print 'Fx', spb.bladestiff1ds[0].Fx
+print 'ys', spb.bladestiff1ds[0].ys
+print 'bf', spb.bladestiff1ds[0].bf
+print 'df', spb.bladestiff1ds[0].df
+print 'E1', spb.bladestiff1ds[0].E1
+print 'F1', spb.bladestiff1ds[0].F1
+print 'S1', spb.bladestiff1ds[0].S1
+print 'Jxx', spb.bladestiff1ds[0].Jxx
 
 print spb.k0.sum()
 print spb.kG0.sum()
