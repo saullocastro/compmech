9f9a65ef250eb3669e8dce3336dfbcc35c884e77
saullocastro <saullogiovani@gmail.com>
2016-01-14 21:39:29 -0200

    TST: new test

--- compmech/panel/tests/test_panel_freq.py
+++ compmech/panel/tests/test_panel_freq.py
@@ -39,5 +39,37 @@ def test_panel_freq():
                 else:
                     assert np.allclose(p.eigvals[0], 39.3147553173)
 
+
+def test_reduced_dof_freq_plate():
+    models = ['plate_clt_donnell_bardell',
+              'cpanel_clt_donnell_bardell']
+    for model in models:
+        atype = 3
+        print('Test reduced_dof solver, atype={0}, model={1}'.format(atype, model))
+        p = Panel()
+        p.model = model
+        p.bc_ssss()
+        p.a = 1.
+        p.b = 0.5
+        p.r = 100.
+        p.alphadeg = 0.
+        p.stack = [0, 90, -45, +45]
+        p.plyt = 1e-3*0.125
+        p.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
+        p.mu = 1.3e3
+        p.m = 11
+        p.n = 12
+        p.Nxx = -60.
+        p.Nyy = -5.
+        p.freq(sparse_solver=True, reduced_dof=False, silent=True,
+               atype=atype)
+        reduced_false = p.eigvals[0]
+        p.freq(sparse_solver=True, reduced_dof=True, silent=True,
+               atype=atype)
+        reduced_true = p.eigvals[0]
+        assert np.isclose(reduced_false, reduced_true)
+
+
 if __name__ == '__main__':
     test_panel_freq()
+    test_reduced_dof_freq_plate()
