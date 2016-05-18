b480526638b69b0cc11364c60ae0297e119313a1
saullocastro <saullogiovani@gmail.com>
2016-05-18 19:45:46 -0300

    TST: updated tests

--- compmech/panel/tests/test_panel_lb_num.py
+++ compmech/panel/tests/test_panel_lb_num.py
@@ -16,7 +16,7 @@ def test_panel_fkG_num():
         p.stack = [0, 90, 90, 0, -45, +45]
         p.plyt = 1e-3*0.125
         p.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
-        p.model = 'plate_clt_donnell_bardell'
+        p.model = model
 
         p.Nxx = -1.
 
@@ -48,11 +48,74 @@ def test_panel_fkG_num():
         p.static(silent=True)
 
         p.lb(silent=True)
-        assert np.isclose(p.eigvals[0], 4.52909113495, atol=0.1, rtol=0)
+        assert np.isclose(p.eigvals[0], 4.52909113495, atol=0.01, rtol=0)
 
         p.lb(silent=True, c=p.analysis.cs[0])
-        assert np.isclose(p.eigvals[0], 4.433932238, atol=0.1, rtol=0)
+        assert np.isclose(p.eigvals[0], 4.433932238, atol=0.01, rtol=0)
+
+
+def test_panel_fkG_num_Fnxny():
+    for model in ['plate_clt_donnell_bardell',
+                  'cpanel_clt_donnell_bardell']:
+        print('Checking fkG_num for model {0}'.format(model))
+        # ssss
+        p = Panel()
+        p.bc_ssss()
+        p.a = 8.
+        p.b = 4.
+        p.r = 1.e8
+        p.stack = [0, 90, 90, 0, -45, +45]
+        p.plyt = 1e-3*0.125
+        p.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
+        p.model = model
+
+        p.Nxx = -1.
+
+        p.m = 8
+        p.n = 9
+
+        p.u1ty = 1
+        p.u2ty = 1
+        p.u2tx = 1
+
+        p.v1tx = 0
+        p.v2tx = 1
+        p.v1ty = 1
+        p.v2ty = 1
+
+        num = 1000.
+        f = 0.
+        for i in range(int(num)):
+            if i == 0 or i == num - 2:
+                fx = p.Nxx*p.b/(num-1.)/2.
+            else:
+                fx = p.Nxx*p.b/(num-1.)
+            p.add_force(x=p.a, y=i*p.b/(num-1.), fx=fx, fy=0., fz=0.,
+                        cte=True)
+            f += fx
+
+        p.static(silent=True)
+        c = p.analysis.cs[0]
+
+        p.lb(silent=True)
+        assert np.isclose(p.eigvals[0], 4.52909113495, atol=0.01, rtol=0)
+
+        nx = 9
+        ny = 9
+        Fnxny = p.F
+        p.lb(silent=True, c=c, Fnxny=Fnxny, nx=nx, ny=ny)
+        assert np.isclose(p.eigvals[0], 4.433932238, atol=0.01, rtol=0)
+
+        nx = 12
+        ny = 10
+        Fnxny = np.array([[p.F]*ny]*nx)
+        p.lb(silent=True, c=c, Fnxny=Fnxny, nx=nx, ny=ny)
+        assert np.isclose(p.eigvals[0], 4.433932238, atol=0.01, rtol=0)
+
+
+#TODO  test_panel_fkG_num_ckL()
 
 
 if __name__ == '__main__':
     test_panel_fkG_num()
+    test_panel_fkG_num_Fnxny()
