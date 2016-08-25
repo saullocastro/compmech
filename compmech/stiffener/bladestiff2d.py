BUG: fixed k0 calculation for base

compmech/stiffener/bladestiff2d.py
@@ -32,7 +32,7 @@ class BladeStiff2D(object):
 
     """
     def __init__(self, bay, mu, panel1, panel2, ys, bb, bf, bstack, bplyts,
-            blaminaprops, fstack, fplyts, flaminaprops, m1=14, n1=11):
+            blaminaprops, fstack, fplyts, flaminaprops, mf=14, nf=11):
         self.bay = bay
         self.panel1 = panel1
         self.panel2 = panel2
@@ -55,7 +55,7 @@ class BladeStiff2D(object):
         self.kM = None
         self.kG0 = None
 
-        self.flange = Panel(m=m1, n=n1, a=bay.a, b=bf, stack=fstack, plyts=fplyts, laminaprops=flaminaprops,
+        self.flange = Panel(m=mf, n=nf, a=bay.a, b=bf, stack=fstack, plyts=fplyts, laminaprops=flaminaprops,
                 model='plate_clt_donnell_bardell')
         self.flange.u1tx = 0.
         self.flange.u1rx = 1.
@@ -121,6 +121,8 @@ class BladeStiff2D(object):
         bay = self.bay
         a = bay.a
         b = bay.b
+        m = bay.m
+        n = bay.n
         r = bay.r if bay.r is not None else 0.
         alphadeg = self.panel1.alphadeg
         alphadeg = alphadeg if alphadeg is not None else 0.
@@ -132,19 +134,23 @@ class BladeStiff2D(object):
             Fsb = self.blam.ABD
             y1 = self.ys - self.bb/2.
             y2 = self.ys + self.bb/2.
-            k0 += basemod.fk0y1y2(y1, y2, a, b, r, alpharad, Fsb, bay.m, bay.n,
-                                 1, 1, 1, 1,
-                                 1, 1, 1, 1,
-                                 1, 1, 1, 1,
-                                 1, 1, 1, 1,
-                                 1, 1, 1, 1,
-                                 1, 1, 1, 1,
-                                 size, 0, 0)
+            k0 += basemod.fk0y1y2(y1, y2, a, b, r, alpharad, Fsb, m, n,
+                                  bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
+                                  bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
+                                  bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
+                                  bay.u1ty, bay.u1ry, bay.u2ty, bay.u2ry,
+                                  bay.v1ty, bay.v1ry, bay.v2ty, bay.v2ry,
+                                  bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
+                                  size, 0, 0)
 
         #TODO add contribution from Nxx_cte from flange and padup
         if self.flange.lam is not None:
             F = self.flange.lam.ABD
             bf = self.flange.b
+            print ('DEBUG')
+            print(a, bf, F, self.flange.m, self.flange.n, size, row0, col0)
+            print(self.flange.get_size())
+            print(self.flange.model)
             k0 += flangemod.fk0(a, bf, 0., 0., F, self.flange.m, self.flange.n,
                            self.flange.u1tx, self.flange.u1rx, self.flange.u2tx, self.flange.u2rx,
                            self.flange.v1tx, self.flange.v1rx, self.flange.v2tx, self.flange.v2rx,
@@ -155,8 +161,16 @@ class BladeStiff2D(object):
                            size, row0, col0)
 
             # connectivity between skin-stiffener flange
+            ycte1 = self.ys
+            ycte2 = 0.
+            flam = self.flange.lam
+            blam = self.blam
+            if blam is None:
+                blam = self.panel1.lam
+            ktbf = (blam.ABD[1, 1] + flam.ABD[1, 1])/(blam.t + flam.t)
+            krbf = (blam.ABD[4, 4] + flam.ABD[4, 4])/(blam.t + flam.t)
             mod = db['bladestiff2d_clt_donnell_bardell']['connections']
-            k0 += mod.fkCss(kt, kr, self.ys, a, b, bay.m, bay.n,
+            k0 += mod.fkCss(ktbf, krbf, self.ys, a, b, m, n,
                             bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
                             bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
                             bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
@@ -164,7 +178,7 @@ class BladeStiff2D(object):
                             bay.v1ty, bay.v1ry, bay.v2ty, bay.v2ry,
                             bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
                             size, 0, 0)
-            k0 += mod.fkCsf(kt, kr, self.ys, a, b, bf, bay.m, bay.n, self.flange.m, self.flange.n,
+            k0 += mod.fkCsf(ktbf, krbf, self.ys, a, b, bf, m, n, self.flange.m, self.flange.n,
                             bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
                             bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
                             bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
@@ -178,7 +192,7 @@ class BladeStiff2D(object):
                             self.flange.v1ty, self.flange.v1ry, self.flange.v2ty, self.flange.v2ry,
                             self.flange.w1ty, self.flange.w1ry, self.flange.w2ty, self.flange.w2ry,
                             size, 0, col0)
-            k0 += mod.fkCff(kt, kr, a, bf, self.flange.m, self.flange.n,
+            k0 += mod.fkCff(ktbf, krbf, a, bf, self.flange.m, self.flange.n,
                             self.flange.u1tx, self.flange.u1rx, self.flange.u2tx, self.flange.u2rx,
                             self.flange.v1tx, self.flange.v1rx, self.flange.v2tx, self.flange.v2rx,
                             self.flange.w1tx, self.flange.w1rx, self.flange.w2tx, self.flange.w2rx,
@@ -237,7 +251,8 @@ class BladeStiff2D(object):
                              size, row0, col0)
 
         if finalize:
-            assert np.any((np.isnan(kG0.data) | np.isinf(kG0.data))) == False
+            assert np.any(np.isnan(kG0.data)) == False
+            assert np.any(np.isinf(kG0.data)) == False
             kG0 = csr_matrix(make_symmetric(kG0))
 
         self.kG0 = kG0
@@ -269,8 +284,8 @@ class BladeStiff2D(object):
         alphadeg = alphadeg if alphadeg is not None else 0.
         alpharad = deg2rad(alphadeg)
 
-        m1 = self.flange.m
-        n1 = self.flange.n
+        mf = self.flange.m
+        nf = self.flange.n
         bf = self.flange.b
 
         kM = 0.
@@ -290,7 +305,7 @@ class BladeStiff2D(object):
                           size, 0, 0)
 
         if self.flange.lam is not None:
-            kM += flangemod.fkM(self.mu, 0., self.hf, a, bf, 0., 0., m1, n1,
+            kM += flangemod.fkM(self.mu, 0., self.hf, a, bf, 0., 0., mf, nf,
                            self.flange.u1tx, self.flange.u1rx, self.flange.u2tx, self.flange.u2rx,
                            self.flange.v1tx, self.flange.v1rx, self.flange.v2tx, self.flange.v2rx,
                            self.flange.w1tx, self.flange.w1rx, self.flange.w2tx, self.flange.w2rx,