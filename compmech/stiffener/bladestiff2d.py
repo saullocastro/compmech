cb6f99b43e7a8b42db1bd66fedacb70920ea8bed
saullocastro <saullogiovani@gmail.com>
2016-08-25 10:28:28 -0300

    ENH: using structural matrices from compmech.panel

--- compmech/stiffener/bladestiff2d.py
+++ compmech/stiffener/bladestiff2d.py
@@ -4,11 +4,11 @@ import gc
 import numpy as np
 from numpy import deg2rad
 
-from . import modelDB
 import compmech.panel.modelDB as panmodelDB
+from compmech.panel import Panel
 from compmech.logger import msg, warn
 from compmech.composite import laminate
-
+from compmech.panel.connections import fkCBFycte11, fkCBFycte12, fkCBFycte22
 
 class BladeStiff2D(object):
     r"""Blade Stiffener using 2D Formulation for Flange
@@ -31,23 +31,16 @@ class BladeStiff2D(object):
 
     """
     def __init__(self, bay, mu, panel1, panel2, ys, bb, bf, bstack, bplyts,
-            blaminaprops, fstack, fplyts, flaminaprops):
+            blaminaprops, fstack, fplyts, flaminaprops, m1=14, n1=11):
         self.bay = bay
         self.panel1 = panel1
         self.panel2 = panel2
-        self.model = 'bladestiff2d_clt_donnell_bardell'
-        self.m1 = 14
-        self.n1 = 11
         self.mu = mu
         self.ys = ys
         self.bb = bb
         self.hb = 0.
-        self.bf = bf
         self.hf = 0.
-        self.forces = []
-
-        self.kt = 1.e8
-        self.kr = 1.e8
+        self.forces_flange = []
 
         self.Nxx = None
         self.Nxy = None
@@ -55,51 +48,49 @@ class BladeStiff2D(object):
         self.bstack = bstack
         self.bplyts = bplyts
         self.blaminaprops = blaminaprops
-        self.fstack = fstack
-        self.fplyts = fplyts
-        self.flaminaprops = flaminaprops
         self.blam = None
-        self.flam = None
 
         self.k0 = None
         self.kM = None
         self.kG0 = None
 
-        self.u1txf = 0.
-        self.u1rxf = 1.
-        self.u2txf = 0.
-        self.u2rxf = 1.
-        self.v1txf = 0.
-        self.v1rxf = 1.
-        self.v2txf = 0.
-        self.v2rxf = 1.
-        self.w1txf = 0.
-        self.w1rxf = 1.
-        self.w2txf = 0.
-        self.w2rxf = 1.
+        self.flange = Panel(m=m1, n=n1, a=bay.a, b=bf, stack=fstack, plyts=fplyts, laminaprops=flaminaprops,
+                model='plate_clt_donnell_bardell')
+        self.flange.u1tx = 0.
+        self.flange.u1rx = 1.
+        self.flange.u2tx = 0.
+        self.flange.u2rx = 1.
+        self.flange.v1tx = 0.
+        self.flange.v1rx = 1.
+        self.flange.v2tx = 0.
+        self.flange.v2rx = 1.
+        self.flange.w1tx = 0.
+        self.flange.w1rx = 1.
+        self.flange.w2tx = 0.
+        self.flange.w2rx = 1.
 
-        self.u1tyf = 1.
-        self.u1ryf = 1.
-        self.u2tyf = 1.
-        self.u2ryf = 1.
-        self.v1tyf = 1.
-        self.v1ryf = 1.
-        self.v2tyf = 1.
-        self.v2ryf = 1.
-        self.w1tyf = 1.
-        self.w1ryf = 1.
-        self.w2tyf = 1.
-        self.w2ryf = 1.
+        self.flange.u1ty = 1.
+        self.flange.u1ry = 1.
+        self.flange.u2ty = 1.
+        self.flange.u2ry = 1.
+        self.flange.v1ty = 1.
+        self.flange.v1ry = 1.
+        self.flange.v2ty = 1.
+        self.flange.v2ry = 1.
+        self.flange.w1ty = 1.
+        self.flange.w1ry = 1.
+        self.flange.w2ty = 1.
+        self.flange.w2ry = 1.
 
         self._rebuild()
 
 
     def _rebuild(self):
-        if self.fstack is not None:
-            self.hf = sum(self.fplyts)
-            self.flam = laminate.read_stack(self.fstack, plyts=self.fplyts,
-                                            laminaprops=self.flaminaprops)
-            self.flam.calc_equivalent_modulus()
+        if self.flange.stack is not None:
+            self.hf = sum(self.flange.plyts)
+            self.flange.lam = laminate.read_stack(self.flange.stack, plyts=self.flange.plyts,
+                                            laminaprops=self.flange.laminaprops)
+            self.flange.lam.calc_equivalent_modulus()
 
         h = 0.5*sum(self.panel1.plyts) + 0.5*sum(self.panel2.plyts)
         if self.bstack is not None:
@@ -123,22 +114,16 @@ class BladeStiff2D(object):
         self._rebuild()
         msg('Calculating k0... ', level=2, silent=silent)
 
-        panmod = panmodelDB.db[self.panel1.model]['matrices']
-        mod = modelDB.db[self.model]['matrices']
+        basemod = panmodelDB.db[self.panel1.model]['matrices']
+        flangemod = panmodelDB.db[self.flange.model]['matrices']
 
         bay = self.bay
         a = bay.a
         b = bay.b
         r = bay.r if bay.r is not None else 0.
-        m = bay.m
-        n = bay.n
         alphadeg = self.panel1.alphadeg
         alphadeg = alphadeg if alphadeg is not None else 0.
         alpharad = deg2rad(alphadeg)
-
-        m1 = self.m1
-        n1 = self.n1
-        bf = self.bf
 
         k0 = 0.
         if self.blam is not None:
@@ -146,60 +131,42 @@ class BladeStiff2D(object):
             Fsb = self.blam.ABD
             y1 = self.ys - self.bb/2.
             y2 = self.ys + self.bb/2.
-            k0 += panmod.fk0y1y2(y1, y2, a, b, r, alpharad, Fsb, m, n,
-                                 bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
-                                 bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
-                                 bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
-                                 bay.u1ty, bay.u1ry, bay.u2ty, bay.u2ry,
-                                 bay.v1ty, bay.v1ry, bay.v2ty, bay.v2ry,
-                                 bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
+
+            print('DEBUG', y1, y2, a, b, r, alpharad, Fsb, bay.m, bay.n)
+            k0 += basemod.fk0y1y2(y1, y2, a, b, r, alpharad, Fsb, bay.m, bay.n,
+                                 1, 1, 1, 1,
+                                 1, 1, 1, 1,
+                                 1, 1, 1, 1,
+                                 1, 1, 1, 1,
+                                 1, 1, 1, 1,
+                                 1, 1, 1, 1,
                                  size, 0, 0)
 
         #TODO add contribution from Nxx_cte from flange and padup
-        if self.flam is not None:
-            kt = self.kt
-            kr = self.kr
-            F = self.flam.ABD
-            k0 += mod.fk0f(a, bf, F, m1, n1,
-                           self.u1txf, self.u1rxf, self.u2txf, self.u2rxf,
-                           self.v1txf, self.v1rxf, self.v2txf, self.v2rxf,
-                           self.w1txf, self.w1rxf, self.w2txf, self.w2rxf,
-                           self.u1tyf, self.u1ryf, self.u2tyf, self.u2ryf,
-                           self.v1tyf, self.v1ryf, self.v2tyf, self.v2ryf,
-                           self.w1tyf, self.w1ryf, self.w2tyf, self.w2ryf,
+        if self.flange.lam is not None:
+            F = self.flange.lam.ABD
+            bf = self.flange.b
+            k0 += flangemod.fk0(a, bf, 0., 0., F, self.flange.m, self.flange.n,
+                           self.flange.u1tx, self.flange.u1rx, self.flange.u2tx, self.flange.u2rx,
+                           self.flange.v1tx, self.flange.v1rx, self.flange.v2tx, self.flange.v2rx,
+                           self.flange.w1tx, self.flange.w1rx, self.flange.w2tx, self.flange.w2rx,
+                           self.flange.u1ty, self.flange.u1ry, self.flange.u2ty, self.flange.u2ry,
+                           self.flange.v1ty, self.flange.v1ry, self.flange.v2ty, self.flange.v2ry,
+                           self.flange.w1ty, self.flange.w1ry, self.flange.w2ty, self.flange.w2ry,
                            size, row0, col0)
 
             # connectivity between skin-stiffener flange
-            k0 += mod.fkCss(kt, kr, self.ys, a, b, m, n,
-                            bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
-                            bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
-                            bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
-                            bay.u1ty, bay.u1ry, bay.u2ty, bay.u2ry,
-                            bay.v1ty, bay.v1ry, bay.v2ty, bay.v2ry,
-                            bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
-                            size, 0, 0)
-            k0 += mod.fkCsf(kt, kr, self.ys, a, b, bf, m, n, m1, n1,
-                            bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
-                            bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
-                            bay.w1tx, bay.w1rx, bay.w2tx, bay.w2rx,
-                            bay.u1ty, bay.u1ry, bay.u2ty, bay.u2ry,
-                            bay.v1ty, bay.v1ry, bay.v2ty, bay.v2ry,
-                            bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
-                            self.u1txf, self.u1rxf, self.u2txf, self.u2rxf,
-                            self.v1txf, self.v1rxf, self.v2txf, self.v2rxf,
-                            self.w1txf, self.w1rxf, self.w2txf, self.w2rxf,
-                            self.u1tyf, self.u1ryf, self.u2tyf, self.u2ryf,
-                            self.v1tyf, self.v1ryf, self.v2tyf, self.v2ryf,
-                            self.w1tyf, self.w1ryf, self.w2tyf, self.w2ryf,
-                            size, 0, col0)
-            k0 += mod.fkCff(kt, kr, a, bf, m1, n1,
-                            self.u1txf, self.u1rxf, self.u2txf, self.u2rxf,
-                            self.v1txf, self.v1rxf, self.v2txf, self.v2rxf,
-                            self.w1txf, self.w1rxf, self.w2txf, self.w2rxf,
-                            self.u1tyf, self.u1ryf, self.u2tyf, self.u2ryf,
-                            self.v1tyf, self.v1ryf, self.v2tyf, self.v2ryf,
-                            self.w1tyf, self.w1ryf, self.w2tyf, self.w2ryf,
-                            size, row0, col0)
+            ycte1 = self.ys
+            ycte2 = 0.
+            flam = self.flange.lam
+            blam = self.blam
+            if blam is None:
+                blam = self.panel1.lam
+            ktbf = (blam.ABD[1, 1] + flam.ABD[1, 1])/(blam.t + flam.t)
+            krbf = (blam.ABD[4, 4] + flam.ABD[4, 4])/(blam.t + flam.t)
+            k0 += fkCBFycte11(ktbf, krbf, bay, ycte1, size, 0, 0)
+            k0 += fkCBFycte12(ktbf, krbf, bay, self.flange, ycte1, ycte2, size, 0, col0)
+            k0 += fkCBFycte22(ktbf, krbf, bay, self.flange, ycte2, size, row0, col0)
 
         if finalize:
             assert np.any(np.isnan(k0.data)) == False
@@ -227,7 +194,7 @@ class BladeStiff2D(object):
         self._rebuild()
         msg('Calculating kG0... ', level=2, silent=silent)
 
-        mod = modelDB.db[self.model]['matrices']
+        flangemod = panmodelDB.db[self.flange.model]['matrices']
 
         bay = self.bay
         a = bay.a
@@ -239,15 +206,15 @@ class BladeStiff2D(object):
             #TODO include kG0 for pad-up (Nxx load that arrives there)
             pass
 
-        if self.flam is not None:
-            F = self.flam.ABD
+        if self.flange.lam is not None:
+            F = self.flange.lam.ABD
             # stiffener flange
 
             Nxx = self.Nxx if self.Nxx is not None else 0.
             Nxy = self.Nxy if self.Nxy is not None else 0.
-            kG0 += mod.fkG0f(Nxx, 0., Nxy, a, self.bf, self.m1, self.n1,
-                             self.w1txf, self.w1rxf, self.w2txf, self.w2rxf,
-                             self.w1tyf, self.w1ryf, self.w2tyf, self.w2ryf,
+            kG0 += flangemod.fkG0(Nxx, 0., Nxy, a, self.flange.b, 0., 0., self.flange.m, self.flange.n,
+                             self.flange.w1tx, self.flange.w1rx, self.flange.w2tx, self.flange.w2rx,
+                             self.flange.w1ty, self.flange.w1ry, self.flange.w2ty, self.flange.w2ry,
                              size, row0, col0)
 
         if finalize:
@@ -270,8 +237,8 @@ class BladeStiff2D(object):
         self._rebuild()
         msg('Calculating kM... ', level=2, silent=silent)
 
-        panmod = panmodelDB.db[self.panel1.model]['matrices']
-        mod = modelDB.db[self.model]['matrices']
+        basemod = panmodelDB.db[self.panel1.model]['matrices']
+        flangemod = panmodelDB.db[self.flange.model]['matrices']
 
         bay = self.bay
         a = bay.a
@@ -283,9 +250,9 @@ class BladeStiff2D(object):
         alphadeg = alphadeg if alphadeg is not None else 0.
         alpharad = deg2rad(alphadeg)
 
-        m1 = self.m1
-        n1 = self.n1
-        bf = self.bf
+        m1 = self.flange.m
+        n1 = self.flange.n
+        bf = self.flange.b
 
         kM = 0.
 
@@ -293,7 +260,7 @@ class BladeStiff2D(object):
             # stiffener pad-up
             y1 = self.ys - self.bb/2.
             y2 = self.ys + self.bb/2.
-            kM += panmod.fkMy1y2(y1, y2, self.mu, self.dpb, self.hb,
+            kM += basemod.fkMy1y2(y1, y2, self.mu, self.dpb, self.hb,
                           a, b, r, alpharad, m, n,
                           bay.u1tx, bay.u1rx, bay.u2tx, bay.u2rx,
                           bay.v1tx, bay.v1rx, bay.v2tx, bay.v2rx,
@@ -303,14 +270,14 @@ class BladeStiff2D(object):
                           bay.w1ty, bay.w1ry, bay.w2ty, bay.w2ry,
                           size, 0, 0)
 
-        if self.flam is not None:
-            kM += mod.fkMf(self.mu, self.hf, a, bf, 0., m1, n1,
-                           self.u1txf, self.u1rxf, self.u2txf, self.u2rxf,
-                           self.v1txf, self.v1rxf, self.v2txf, self.v2rxf,
-                           self.w1txf, self.w1rxf, self.w2txf, self.w2rxf,
-                           self.u1tyf, self.u1ryf, self.u2tyf, self.u2ryf,
-                           self.v1tyf, self.v1ryf, self.v2tyf, self.v2ryf,
-                           self.w1tyf, self.w1ryf, self.w2tyf, self.w2ryf,
+        if self.flange.lam is not None:
+            kM += flangemod.fkM(self.mu, 0., self.hf, a, bf, 0., 0., m1, n1,
+                           self.flange.u1tx, self.flange.u1rx, self.flange.u2tx, self.flange.u2rx,
+                           self.flange.v1tx, self.flange.v1rx, self.flange.v2tx, self.flange.v2rx,
+                           self.flange.w1tx, self.flange.w1rx, self.flange.w2tx, self.flange.w2rx,
+                           self.flange.u1ty, self.flange.u1ry, self.flange.u2ty, self.flange.u2ry,
+                           self.flange.v1ty, self.flange.v1ry, self.flange.v2ty, self.flange.v2ry,
+                           self.flange.w1ty, self.flange.w1ry, self.flange.w2ty, self.flange.w2ry,
                            size, row0, col0)
 
         if finalize:
