8faf71bcf0db1af5c29acca0e0f7c31cd6b76fdc
saullocastro <saullogiovani@gmail.com>
2016-09-27 22:28:25 -0300

    MAINT

--- compmech/panel/assembly/__init__.py
+++ compmech/panel/assembly/__init__.py
@@ -13,6 +13,8 @@ Assembly of Plate and Panel Models (:mod:`compmech.panel.assembly`)
 from __future__ import absolute_import
 
 from . assembly import PanelAssembly
-from . tstiff2d_1stiff_freq import tstiff2d_1stiff_freq
-from . tstiff2d_1stiff_compression import tstiff2d_1stiff_compression
-from . tstiff2d_1stiff_flutter import tstiff2d_1stiff_flutter
+from . tstiff2d_1stiff_freq import *
+from . tstiff2d_1stiff_compression import *
+from . tstiff2d_1stiff_flutter import *
+from . cylinder import *
+from . cylinder_blade_stiffened import *
