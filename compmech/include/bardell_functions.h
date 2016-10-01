a6cb0a5283f9cdd35db431a222f902ecdeb88dc6
saullocastro <saullogiovani@gmail.com>
2016-10-01 16:52:30 -0300

    DEV: func calc_vec_fxixi

--- compmech/include/bardell_functions.h
+++ compmech/include/bardell_functions.h
@@ -13,6 +13,9 @@ IMPORTIT void calc_vec_f(double *f, double xi,
 IMPORTIT void calc_vec_fxi(double *fxi, double xi,
         double xi1t, double xi1r,double xi2t, double xi2r);
 
+IMPORTIT void calc_vec_fxixi(double *fxixi, double xi,
+        double xi1t, double xi1r,double xi2t, double xi2r);
+
 IMPORTIT double calc_f(int i, double xi,
         double xi1t, double xi1r, double xi2t, double xi2r);
 
