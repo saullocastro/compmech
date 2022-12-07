bee6466e3bb7c52f9f95c391fb28a0fc9cfa2ef6
saullocastro <saullogiovani@gmail.com>
2022-12-07 07:13:55 +0100

    BLD: attempting faster py.test runtime

--- setup.py
+++ setup.py
@@ -116,20 +116,23 @@ package_data = {
 link_args = []
 if os.name == 'nt': # Windows
     if os.environ.get('CYTHON_TRACE_NOGIL') is not None:
-        compiler_args = ['/openmp', '/Od']
+        #compiler_args = ['/openmp', '/Od']
+        compiler_args = ['/openmp']
     else:
         compiler_args = ['/openmp']
     compiler_args_NL = compiler_args + ['/fp:fast']
 elif os.name == 'posix': # MAC-OS
     if os.environ.get('CYTHON_TRACE_NOGIL') is not None:
-        compiler_args = ['-fopenmp', '-O0']
+        #compiler_args = ['-fopenmp', '-O0']
+        compiler_args = ['-fopenmp']
     else:
         compiler_args = ['-fopenmp']
     link_args = ['-fopenmp']
     compiler_args_NL = compiler_args + ['-ffast-math']
 else: # Linux
     if os.environ.get('CYTHON_TRACE_NOGIL') is not None:
-        compiler_args = ['-fopenmp', '-O0']
+        #compiler_args = ['-fopenmp', '-O0']
+        compiler_args = ['-fopenmp']
     else:
         compiler_args = ['-fopenmp']
     link_args = ['-fopenmp']
